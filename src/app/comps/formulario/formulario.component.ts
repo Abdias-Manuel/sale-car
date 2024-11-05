50b1f4f80a9adcad70aa15f2f0f3d68f7c0e6e1b
abdias-manuel <abdiase8@gmail.com>
2024-11-05 12:51:01 +0100

    formulario meio pronto

--- src/app/comps/formulario/formulario.component.ts
+++ src/app/comps/formulario/formulario.component.ts
@@ -1,12 +1,46 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './formulario.component.html',
   styleUrl: './formulario.component.css'
 })
 export class FormularioComponent {
-
+  questoes:any[] =[
+    {
+      pergunta:'nome completo',
+      typ:'text',
+      resp:'',
+      preenchido:false,
+    },
+    {
+      pergunta:'nº de identidade',
+      typ:'text',
+      resp:'',
+      preenchido:false,
+    },
+    {
+      pergunta:'data de nascimento',
+      typ:'date',
+      resp:'',
+      preenchido:false,
+    },
+    {
+      pergunta:'contacto telefónico',
+      typ:'number',
+      resp:'',
+      whats:false,
+      preenchido:false,
+    },
+    {
+      pergunta:'correio electronico',
+      typ:'e-mail',
+      resp:'',
+      preenchido:false,
+    }
+  ];
 }
