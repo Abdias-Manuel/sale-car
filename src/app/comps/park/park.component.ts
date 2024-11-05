dadcfd447aee13773e787c30610d45c515cfba08
abdias-manuel <abdiase8@gmail.com>
2024-11-05 09:39:03 +0100

    array de cards criado

--- src/app/comps/park/park.component.ts
+++ src/app/comps/park/park.component.ts
@@ -8,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrl: './park.component.css'
 })
 export class ParkComponent {
-  
+  cards:object[] = [
+    { 
+      img:'images/suziki-s-presso.png',
+      tit:'Suziki S-PRESSO',
+      tex:'O Suzuki S-Presso é um compacto urbano que combina praticidade e estilo em um único veículo. Equipado com um motor de 1.0 litro, ele oferece desempenho eficiente e economia de combustiv...'
+    },
+    { 
+      img:'images/suziki-s-presso.png',
+      tit:'Suziki S-PRESSO',
+      tex:'O Suzuki S-Presso é um compacto urbano que combina praticidade e estilo em um único veículo. Equipado com um motor de 1.0 litro, ele oferece desempenho eficiente e economia de combustiv...'
+    }
+  ];
 }
